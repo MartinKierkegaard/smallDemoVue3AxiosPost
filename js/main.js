0bdfea7dce76f05ec15e40d79473b079b5b86bf5
Martin Kierkegaard <MARK@zealand.dk>
2024-11-18 09:42:58 +0100

    tilføjet baseUri + Steps til opgaven

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const baseUri = "http://localhost:5180/api/Cars"
+
+// STEPS
+// 1 Tilføj variabler til data i js - 
+// 2 Tilføj input felter som twoway pinder til variaber - 
+// 3 Test at twoway binding virker med {{}} - commit
+// 4 Tilføj knap og click event 
+// 5 Test knap - commit
+// 6 Tilføj axios post kald
+// 7 test at .then virker - commit
+// 8 test at .catch virker - commit 
+// 9 Tilføj statuskode vises i html 
+// FIN
+
+
 const app = Vue.createApp({
     data() {
         return {
