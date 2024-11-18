c4e03d995a6ff8021134afc5ac9032d050ab78f6
Martin Kierkegaard <MARK@zealand.dk>
2024-11-18 09:31:55 +0100

    tilføjet gemBil metode med axios kalde, statuskode bliver vist i html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const baseUri = "http://localhost:5180/api/Cars"
+
 const app = Vue.createApp({
     data() {
         return {
@@ -6,11 +8,28 @@ const app = Vue.createApp({
             carVendor:"",
             carModel:"",
             carPrice:"",
+            statusCode:"",
+            statusText:"",
 
         }
     },
     methods: {
         gemBil(){
+            console.log("Er i gem bil funktion")
+            axios.post(baseUri,
+                {"id":this.carId,"vendor":this.carVendor,"model":this.carModel,"price":this.carPrice}
+            )
+            .then(response => {
+                console.log("er i then i gemBil")
+                this.statusCode=response.status
+                console.log(response)
+            })
+            .catch(error => {
+                console.log("er i Catch i gemBil funktion")
+                console.log(error)
+                this.statusCode=error.response.status
+                // console.log(error.response.status)
+            })
 
         },
     },
